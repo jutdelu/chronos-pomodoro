cda7146a22988cb26ddbad191d9043c5f35926cd
juliana.torres <juliana.torres@novoseguros.com.br>
2025-07-04 14:00:47 -0300

    feat: including context on aplication - part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { TaskContextProvider } from './contexts/TaskContext';
 import { Home } from './pages/Home';
 import './styles/global.css';
 import './styles/theme.css';
 
 export const App = () => {
-   return <Home />;
-  
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 };
--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,5 +1,7 @@
+import { useTaskContext } from '../../contexts/TaskContext';
 import styles from './styles.module.css';
 
 export const CountDown = () => {
-  return <div className={styles.container}>00:00</div>;
+  const { state } = useTaskContext();
+  return <div className={styles.container}>{state.formattedSecondsRemaining}</div>;
 };
