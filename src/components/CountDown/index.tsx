0e4f3176ad82ddee0a903db35ed83d8a52536894
juliana.torres <juliana.torres@novoseguros.com.br>
2025-07-14 15:29:54 -0300

    refactor: adjusting context files

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,4 +1,5 @@
-import { useTaskContext } from '../../contexts/TaskContext';
+
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import styles from './styles.module.css';
 
 export const CountDown = () => {
